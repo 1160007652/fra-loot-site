dae5ae7ee38da47da18d2c7e346fd0dcbadfea25
muniz <1160007652@qq.com>
2021-09-14 17:43:28 +0800

    feat: site

---
+++ src/pages/Action/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { Button, Input, message } from 'antd';
+import { useWeb3React } from '@web3-react/core';
+
+import NFTBox from '_components/NFTBox';
+import ConnectWallet from '_components/ConnectWallet';
+
+import services from '_src/services';
+
+import './index.less';
+import { useHistory } from 'react-router';
+import pageURL from '_constants/pageURL';
+
+function Action() {
+  const { account } = useWeb3React();
+  const hirstory = useHistory();
+  const [word, setWord] = useState('');
+  const [nftList, setNftList] = useState([]);
+
+  useEffect(() => {
+    services.lootServer
+      .getNFTList()
+      .then((data) => {
+        console.log(data);
+        setNftList(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [account]);
+
+  function onClaimWord(e) {
+    setWord(e.target.value);
+  }
+
+  async function onClaimSubmit() {
+    if (word.trim() === '') {
+      message.info('Can not be empty');
+      return;
+    }
+    try {
+      const isExist = await services.lootServer.ownerOf(word);
+      if (isExist) {
+        await services.lootServer.claim(word);
+        message.success('mint successed');
+      } else {
+        message.warn('NFT has been claimed');
+      }
+    } catch (err) {
+      message.error(err.message);
+    }
+  }
+
+  return (
+    <div className="action-page">
+      <div className="wallet_box">
+        <div className="btn_back" onClick={() => hirstory.push(pageURL.home)}>
+          Back
+        </div>
+        <ConnectWallet />
+      </div>
+      <div className="title">Loot</div>
+      <div className="sub_info"> Findora Loot Contract ` Example</div>
+      <div className="input_box">
+        <Input placeholder="Input ID（ 0 ~ 8000 ）to get a Legoot" onChange={onClaimWord} className="word-input" />
+        <div className="btn_box">
+          <Button onClick={onClaimSubmit}>Mint</Button>
+        </div>
+      </div>
+      <div className="continer">
+        {nftList.length > 0 &&
+          nftList.map((item) => {
+            return <NFTBox key={item.id} data={item} />;
+          })}
+      </div>
+    </div>
+  );
+}
+export default Action;
---
+++ src/services/web3.ts
@@ -0,0 +1,52 @@
+import Web3 from 'web3';
+import BigNumber from 'bignumber.js';
+import Tokens from '_constants/tokens';
+
+const LootABI = require('_abis/loot.json');
+
+const web3 = new Web3(Web3.givenProvider);
+
+const getContract = (address: string, ABI: any, options) => {
+  return new web3.eth.Contract(ABI, address, options);
+};
+
+const getLootContract = () => {
+  return getContract(Tokens.LegootAddress, LootABI, { gasLimit: '8000000' });
+};
+
+const getBalance = async (address: string) => {
+  let balance = await web3.eth.getBalance(address);
+  return balance;
+};
+
+const getDefaultAccount = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+  if (accounts.length > 0) {
+    return accounts[0];
+  }
+  return '';
+};
+
+const gasOptions = async (params = {}) => {
+  // const gasLimit = Web3.utils.toHex(8000000);
+  // const gasPrice = Web3.utils.toHex(1000000);
+  const from = await getDefaultAccount();
+  return {
+    from,
+    // gasLimit,
+    // gasPrice,
+    ...params,
+  };
+};
+
+const toDecimals = async (contract: any, amount: string, decimals: string, flag: 'timse' | 'div') => {
+  const ten = new BigNumber(10);
+  // const decimals = await contract.methods.decimals().call();
+  const power = ten.exponentiatedBy(decimals);
+  const amountBN = new BigNumber(amount);
+  console.log(amountBN.toString(), decimals.toString());
+  return flag === 'timse' ? amountBN.times(power) : amountBN.div(power);
+};
+
+export { web3, getContract, getLootContract, gasOptions, getDefaultAccount, getBalance, toDecimals };
